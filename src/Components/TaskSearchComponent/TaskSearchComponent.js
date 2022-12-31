3bf433eb0e3cee30917022fe900e6429609e6d60
David Asaaf Vergara <as.f09942@gmail.com>
2022-12-31 18:38:40 -0500

    Update TaskSearchComponent.js
    React.useState is used to change elements inside the component

--- src/Components/TaskSearchComponent/TaskSearchComponent.js
+++ src/Components/TaskSearchComponent/TaskSearchComponent.js
@@ -1,18 +1,22 @@
+import React from 'react';
 import './TaskSearchComponent.css';
 
 function TaskSearchComponent() {
+    const [searchValue, setSearchValue] = React.useState();
+
     const detectChanges = (change) => {
         console.log(change.target.value);
+        setSearchValue(change.target.value);
     };
 
-    return (
+    return [
         <div className='search-container'>
             <div className='title-section'>
                 <h1>Lista</h1>
                 <span>De tareas</span>
             </div>
             <div className='search-section'>
-                <input type="text" name='search' id='search' className='search' placeholder='Buscar...' onChange={detectChanges } />
+                <input type="text" name='search' id='search' className='search' placeholder='Buscar...' onChange={detectChanges} value={searchValue} />
                 <button className='btn-search'>
                     <span className="material-symbols-outlined">
                         search
@@ -20,7 +24,7 @@ function TaskSearchComponent() {
                 </button>
             </div>
         </div>
-    );
+    ];
 }
 
 export { TaskSearchComponent };
