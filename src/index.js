8766724d0172bfbedd35d53fd733d13432241c62
David Asaaf Vergara <as.f09942@gmail.com>
2023-01-06 18:23:59 -0500

    Background Component Update
    Be better at file order

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import AppController from './Components/AppComponent/AppController';
-import { BackgroundComponent } from './Components/BackgroundComponent/BackgroundComponent';
+import { BackgroundController } from './Components/BackgroundComponent/BackgroundController';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.Fragment>
         <AppController />
-        <BackgroundComponent />
+        <BackgroundController />
     </React.Fragment>
 );
